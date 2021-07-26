b72d751d4074abb06f7c7e9bd2c7c82f547b8bb1
Song <w2033@e-mirim.hs.kr>
2021-07-26 13:39:59 +0900

    Edit MainActivity.java(Remove Item) Commit

--- app/src/main/java/kr/hs/emirim/w2033/ex4_5/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/w2033/ex4_5/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,12 +15,13 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
     ArrayList<String> arrList;
     ArrayAdapter<String> adapter;
-    EditText edtItem;
+    EditText editItem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editItem = findViewById(R.id.edit_item);
         arrList = new ArrayList<String>();
         ListView list1 = findViewById(R.id.list1);
         adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, arrList);
@@ -28,8 +30,17 @@ public class MainActivity extends AppCompatActivity {
         btnAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                arrList.add(edtItem.getText().toString());
+                arrList.add(editItem.getText().toString());
                 adapter.notifyDataSetChanged();
+                editItem.setText("");
+            }
+        });
+        list1.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                arrList.remove(position);
+                adapter.notifyDataSetChanged();
+                return false;
             }
         });
 
