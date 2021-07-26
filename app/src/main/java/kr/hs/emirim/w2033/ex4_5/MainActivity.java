08a5784bbb796cb665a21cdcd49d9fbe7e0e3e5c
Song <w2033@e-mirim.hs.kr>
2021-07-26 13:31:13 +0900

    Edit MainActivity.java(Add Item) Commit

--- app/src/main/java/kr/hs/emirim/w2033/ex4_5/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/w2033/ex4_5/MainActivity.java
@@ -3,12 +3,35 @@ package kr.hs.emirim.w2033.ex4_5;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    ArrayList<String> arrList;
+    ArrayAdapter<String> adapter;
+    EditText edtItem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        arrList = new ArrayList<String>();
+        ListView list1 = findViewById(R.id.list1);
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, arrList);
+        list1.setAdapter(adapter);
+        Button btnAdd = findViewById(R.id.btn_add);
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                arrList.add(edtItem.getText().toString());
+                adapter.notifyDataSetChanged();
+            }
+        });
+
     }
 }
